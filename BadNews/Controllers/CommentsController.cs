bcf952438706ed454a665b65a28ab15d3584b78d
Grinik767 <grigorikiseleff@yandex.ru>
2025-11-18 15:03:10 +0500

    1.1

---
+++ BadNews/Controllers/CommentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers;
+
+[ApiController]
+public class CommentsController(CommentsRepository commentsRepository) : ControllerBase
+{
+    [HttpGet("api/news/{newsId:guid}/comments")]
+    public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+    {
+        var comments = commentsRepository.GetComments(newsId)
+            .Select(c => new CommentDto { User = c.User, Value = c.Value })
+            .ToArray();
+
+        return new CommentsDto { NewsId = newsId, Comments = comments };
+    }
+}
